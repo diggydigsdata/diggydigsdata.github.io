c0eaeb4abf3c7055cc0e156b3d67f7d9522df956
diggydigsdata <eoindignan@gmail.com>
2025-04-23 10:57:37 +0000

    rebuilding container for jupyter notebook requirements

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -16,3 +16,6 @@ sed -i -E "s/^(plugins=\()(git)(\))/\1\2 zsh-syntax-highlighting zsh-autosuggest
 
 # Avoid git log use less
 echo -e "\nunset LESS" >>~/.zshrc
+
+# Install Python dependencies from requirements.txt
+pip install -r /workspaces/diggydigsdata.github.io/requirements.txt
